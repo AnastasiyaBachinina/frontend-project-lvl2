c7be73ccfb82cdd2ef2821bedd175189f000b7ea
Sergey Bachinin <bachinin_sergey@mail.ru>
2022-08-01 11:21:48 +0300

    Added JStest to index/test.js

---
+++ __tests__/index.test.js
@@ -0,0 +1,16 @@
+import genDiff from '../src/index.js';
+
+const result = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+
+test('genDiff JSON', () => {
+  const filepath1 = './__fixtures__/file1.json';
+  const filepath2 = './__fixtures__/file2.json';
+  expect(genDiff(filepath1, filepath2)).toEqual(result);
+});
