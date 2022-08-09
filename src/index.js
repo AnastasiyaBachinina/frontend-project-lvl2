9a973b82781c58e78882f38d05b501162dfb1e9d
Sergey Bachinin <bachinin_sergey@mail.ru>
2022-08-09 21:32:34 +0300

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -2,33 +2,44 @@ import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
 import parseFile from './parsers.js';
+import format from './formatter/stylish.js';
 
-const readFile = (pathToFile) => {
-  const format = path.extname(pathToFile);
-  const contentFile = parseFile(fs.readFileSync(pathToFile, 'utf8'), format);
-  return contentFile;
+const buildTree = (data1, data2) => {
+  const keys = Object.keys({ ...data1, ...data2 });
+  const sortedKeys = _.sortBy(keys);
+  return sortedKeys.map((key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
+    if (!_.has(data1, key)) {
+      return { type: 'added', key, value: value2 };
+    }
+    if (!_.has(data2, key)) {
+      return { type: 'removed', key, value: value1 };
+    }
+    if (_.isObject(value1) && _.isObject(value2)) {
+      return { type: 'nested', key, children: buildTree(value1, value2) };
+    }
+    if (!_.isEqual(value1, value2)) {
+      return {
+        type: 'updated', key, value1, value2,
+      };
+    }
+    return { type: 'notChanged', key, value: value1 };
+  });
 };
 
-const genDiff = (pathToFile1, pathToFile2) => {
-  const file1 = readFile(pathToFile1);
-  const file2 = readFile(pathToFile2);
+const readFile = (filename) => fs.readFileSync(path.resolve(process.cwd(), filename.trim()), 'utf-8');
+const extractFormat = (filename) => path.extname(filename).slice(1);
 
-  const keys = Object.keys({ ...file1, ...file2 }).sort();
-
-  const getDistinction = keys.map((key) => {
-    if (!_.has(file2, key)) {
-      return `- ${key}: ${file1[key]}`;
-    }
-    if (!_.has(file1, key)) {
-      return `+ ${key}: ${file2[key]}`;
-    }
-    if (file1[key] === file2[key]) {
-      return `  ${key}: ${file1[key]}`;
-    }
-    return `- ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}`;
-  }).join('\n  ');
-
-  return `{\n  ${getDistinction}\n}`;
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
+  const fileformat1 = extractFormat(filepath1);
+  const fileformat2 = extractFormat(filepath2);
+  const fileContent1 = readFile(filepath1);
+  const fileContent2 = readFile(filepath2);
+  const data1 = parseFile(fileformat1, fileContent1);
+  const data2 = parseFile(fileformat2, fileContent2);
+  const innerTree = buildTree(data1, data2);
+  return format(innerTree, formatName);
 };
 
 export default genDiff;
