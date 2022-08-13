5c7457b4bc7de94515e579d8c72ac6a6d5b0656d
Sergey Bachinin <bachinin_sergey@mail.ru>
2022-08-13 23:04:39 +0300

    Corrected file

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -3,12 +3,10 @@ import plain from './plain.js';
 
 const format = (tree, formatName) => {
   switch (formatName) {
-    case 'stylish': {
+    case 'stylish':
       return stylish(tree);
-    }
-    case 'plain': {
+    case 'plain':
       return plain(tree);
-    }
     case 'json':
       return JSON.stringify(tree, null, 4);
     default:
